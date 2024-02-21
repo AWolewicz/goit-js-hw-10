bfd1df538ad2feb417d9051a4f0ceb292b898427
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-02-21 19:08:19 +0100

    up

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 
 try {
   loader.classList.remove('hidden');
